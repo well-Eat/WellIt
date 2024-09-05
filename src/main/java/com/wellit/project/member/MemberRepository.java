1020ca52f509d5d9ff08c6b7eba1c3f0ecef7942
haeminLee-web <rufp9408@gmail.com>
2024-09-05 18:36:55 +0900

    minsu 변경사항 올커밋

---
+++ src/main/java/com/wellit/project/member/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.wellit.project.member;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, String>{
+	Optional<Member> findByMemberId(String memberId);
+	boolean existsByMemberId(String memberId);
+}
---
+++ src/main/java/com/wellit/project/member/MemberSecurityService.java
@@ -0,0 +1,40 @@
+package com.wellit.project.member;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class MemberSecurityService implements UserDetailsService{
+	
+	private final MemberRepository memberRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+		System.out.println("Requested memberId: " + memberId);  // 요청된 memberId 출력
+		Optional<Member> _member = this.memberRepository.findByMemberId(memberId);
+		System.out.println(_member);
+		if(_member.isEmpty()) {
+			throw new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다.");
+		}
+		Member member = _member.get();
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		if("admin".equals(memberId)) {
+			authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
+		}else {
+			authorities.add(new SimpleGrantedAuthority(MemberRole.USER.getValue()));
+		}
+		return new User(member.getMemberId(), member.getMemberPassword(), authorities);	
+	}
+}
