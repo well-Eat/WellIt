e572b12b8c027d8cca6564d3f7b2b459642b7e9e
SaeronJang <yoonseo35@gmail.com>
2024-09-23 14:38:48 +0900

    => 패치 후 충돌 수정
    * 주문서 생성 30분간 결제 대기 -> 주문서 삭제 및 재고 복구
    
    * email js추가
    
    * 순환참조 일부 dto로 변환(poDetailForm)
    * 상품 리뷰, 찜, 조회수 추가
    
    * admin order 캘린더

--- src/main/java/com/wellit/project/MainController.java
+++ src/main/java/com/wellit/project/MainController.java
@@ -23,11 +23,6 @@ public class MainController {
         model.addAttribute("stores", topStores); // 모델에 가게 목록 추가
         return "index"; // 인덱스 페이지로 이동
     }
-	
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
 
 	// 로그인 오류 창 후 로그인 페이지로 리다이렉트
 	@GetMapping("/login-alert")
